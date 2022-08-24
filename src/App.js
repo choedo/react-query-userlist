4b3beb71b69e59c46cbfde9547d2e27bf21943a5
Choi Doyeong <91508019+choedo@users.noreply.github.com>
2022-08-24 18:10:36 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from "react-query";
+import RQ from "./components/RQ";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 리액트 쿼리 초기 설정 */}
+      <QueryClientProvider client={queryClient}>
+        <RQ />
+      </QueryClientProvider>
     </div>
   );
 }
